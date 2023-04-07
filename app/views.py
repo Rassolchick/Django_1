3fbc812ed6855d95f1bd4d7b7ca3792bb68bda37
Ilya Rassolov <lexxi.s.mom1992@gmail.com>
2023-04-07 09:39:46 +0300

    second commit

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+import os
+from datetime import datetime
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }        
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):    
+    current_time = datetime.now()
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    path = '.'
+    ls_dir = '<br>'.join(os.listdir(path))
+    return HttpResponse(ls_dir)
